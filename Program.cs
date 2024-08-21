e3c5839280e4f3fe693d2256bb98d193996f4950
ItsMdAftab <mdaftab01@outlook.com>
2024-08-21 15:53:10 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿/* #1. distance calculator 
+ using System;
+class Program
+{
+    static void Main()
+    {
+        double speed, time, distance;
+
+        Console.Write("Enter speed (in meters per second): ");
+        speed = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter time (in seconds): ");
+        time = Convert.ToDouble(Console.ReadLine());
+
+        distance = speed * time;
+
+        Console.WriteLine("Distance traveled: " + distance + " meters");
+    }
+}
+*/ 
+using System;
+class Program
+{
+    static void Main()
+    {
+        double number, cubroot;
+        Console.WriteLine("Enter The Number : ");
+         number =Convert.ToDouble( Console.ReadLine());
+         cubroot=Math.Cbrt(number);
+        Console.WriteLine($"The cube root of {number} is : {cubroot}");
+    }
+}
+
+
