f8a321df1d5430240ee170bf720a8330b73a378c
Muhammad Aftab <mdaftab01@outlook.com>
2024-09-11 14:41:50 +0530

    Add files via upload

---
+++ week-3/3.1Find_the_Bomb/3.4Track_The_Robot/3.4Track_The_Robot/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+class Program
+{
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+        String[] s1 = s.Split(',');
+        int x = 0;
+        int y = 0;
+        foreach (string s2 in s1)
+        {
+            String[] s3 = s2.Split(' ');
+            string part = s3[0];
+            int value=int.Parse(s3[1]);
+            
+            
+            switch (part.ToLower())
+            {
+                case "right":
+                    x += value;
+                    break;
+                case "left":
+                    x -= value;
+                    break;
+                case "up":
+                    y += value;
+                    break;
+                case "down":
+                    y -= value;
+                    break;
+
+            }
+
+        }
+        Console.WriteLine($"Final coordinates:({x},{y})");
+
+
+    }
+
+
+
+
+}
