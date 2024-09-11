5c55b37dddcdc48aa75713d59a055213c164563e
Muhammad Aftab <mdaftab01@outlook.com>
2024-09-11 13:48:27 +0530

    Add files via upload

---
+++ week-3/3.1Find_the_Bomb/3.1Find_the_Bomb/Program.cs
@@ -0,0 +1,26 @@
+﻿using System; 
+class Program 
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter the String : ");
+        string s1 = Console.ReadLine();
+        string k = bomb(s1);
+        Console.WriteLine(k); 
+
+    }
+    static string bomb(String s)
+    {
+        if (s.ToLower().Contains("bomb"))
+        {
+            return "Duck!!!";
+
+            
+        }
+        else
+        {
+            return "There is no bomb , relax.";
+        }
+
+    }
+}
