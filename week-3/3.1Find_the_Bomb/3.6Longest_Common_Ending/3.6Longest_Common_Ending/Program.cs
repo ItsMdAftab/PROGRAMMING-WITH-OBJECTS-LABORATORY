3c5ff0b2d03a35fd168158a8c12b1b8e62be0db4
Muhammad Aftab <mdaftab01@outlook.com>
2024-09-11 15:18:28 +0530

    Add files via upload

---
+++ week-3/3.1Find_the_Bomb/3.6Longest_Common_Ending/3.6Longest_Common_Ending/Program.cs
@@ -0,0 +1,26 @@
+﻿using System; 
+class Program
+{
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+        String[] s1 = s.ToLower().Split(',');
+        string part=s1[0];
+        string part1=s1[1];
+        int minlength=Math.Min(part.Length, part1.Length);
+        string commonending = "";
+        for (int i = 1; i <=minlength; i++)
+        {
+            if (part[part.Length - i] == part1[part1.Length-i])
+            {
+                commonending += part[part.Length-i];
+            }
+            else
+            {
+                break;
+            }
+
+        }
+        Console.WriteLine(commonending);
+    }
+}
