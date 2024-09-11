27f832f2c84eb1df5d45c87e021c32bb273471e0
Muhammad Aftab <mdaftab01@outlook.com>
2024-09-11 14:18:43 +0530

    Add files via upload

---
+++ week-3/3.1Find_the_Bomb/3.3A_Week_Later/3.3A_Week_Later/Program.cs
@@ -0,0 +1,11 @@
+﻿using System; 
+class Program
+{
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+        String[] s1 = s.Split('/');
+        int n = int.Parse(s1[0]);
+        Console.WriteLine($"{n + 7}/{s1[1]}/{s1[2]}");
+    }
+}
