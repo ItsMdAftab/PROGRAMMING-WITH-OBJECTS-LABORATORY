6f4dcb9dc84f19a9bac5aaee583b4471a492905a
Muhammad Aftab <mdaftab01@outlook.com>
2024-09-11 14:12:30 +0530

    Add files via upload

---
+++ week-3/3.1Find_the_Bomb/3.2FindingNemo/3.2FindingNemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.SqlTypes;
+class Program
+{
+    public static void Main() 
+    {
+        string s1 = Console.ReadLine();
+        string [] s2 = s1.Split(' ');
+        int n = s2.Length; 
+        for(int i = 0; i < n; i++)
+        {
+            if (s2[i] == "Nemo")
+            {
+                Console.WriteLine($"I found Nemo at {i + 1}");
+            }
+            
+           
+
+        }
+
+    }
+}
