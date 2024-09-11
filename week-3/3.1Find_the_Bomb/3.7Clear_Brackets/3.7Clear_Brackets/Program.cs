7d64f0b4c5806dcca9e300dba405322dd0ed9b8a
Muhammad Aftab <mdaftab01@outlook.com>
2024-09-11 15:56:24 +0530

    Add files via upload

---
+++ week-3/3.1Find_the_Bomb/3.7Clear_Brackets/3.7Clear_Brackets/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    public static void Main()
+    {
+        string s = Console.ReadLine(); 
+        Stack<int> stack = new Stack<int>();
+        foreach(char a in s)
+        {
+            if (a == '(')
+            {
+                stack.Push(a);
+            }
+            else if (a == ')')
+            {
+                if (stack.Count == 0)
+                {
+                    Console.WriteLine("false");
+                }
+                stack.Pop();
+            }
+
+        }
+        if (stack.Count == 0)
+        {
+            Console.WriteLine("true");
+        }
+
+
+        
+
+    }
+}
